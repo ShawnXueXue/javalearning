70f7bd26e590e114a877f9ab5725a38a8dd98dff
wgxue <wgxue@ctrip.com>
2018-07-04 15:38:38 +0800

    atomic

---
+++ src/test/java/shawn/keeplearning/bazza/AtomicReferenceTest.java
@@ -0,0 +1,23 @@
+package shawn.keeplearning.bazza;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @date 2018/7/4 14:31
+ * Author: Shawn
+ * Contact me: shawnglhf@gmail.com
+ */
+public class AtomicReferenceTest {
+
+	private static String initStr = "init";
+
+	@Test
+	public void t1() {
+		AtomicReference<String> aStr = new AtomicReference<String>(initStr);
+		System.out.println(aStr.getAndSet("new"));
+		System.out.println(aStr.compareAndSet("new", "new1"));
+		System.out.println(aStr);
+	}
+}
